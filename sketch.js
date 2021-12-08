0e29bc6eb433cfc8d7c8634a194271914cd64325
R4ND0MPR0GR4M3R <94574805+R4ND0MPR0GR4M3R@users.noreply.github.com>
2021-12-08 19:19:37 -0300

    Upload2
    tá funfando agora

--- sketch.js
+++ sketch.js
@@ -1,35 +1,28 @@
 var sea, seaImg;
 var ship, shipImg;
-var trueSea;
 
 function preload(){
 shipImg = loadAnimation("ship-2.png","ship-1.png","ship-3.png","ship-1.png","ship-2.png","ship-4.png");
-seaImg = loadAnimation("sea.png");
+seaImg = loadImage("sea.png");
 }
 
 function setup(){
-  createCanvas(600,200)
+  createCanvas(800,800)
   
   //criação do mar
   sea = createSprite(200,200,50,50);
   sea.addImage("sea.png",seaImg);
 
   //criação do barco
-  ship = createSprite(50,180,50,50);
+  ship = createSprite(150,500,50,50);
   ship.addAnimation("barquinho",shipImg);
+  ship.scale=0.35;
   
   bordas = createEdgeSprites();
-
-  //ship lock
-  trueSea = createSprite(300,186,600,10)
-  trueSea.visible = false;
 }
 
 function draw() {
   background("blue");
-
-  //colisão com o lock
-  ship.collide(trueSea);
 
   //velocidad do mar
   sea.velocityX=-2
